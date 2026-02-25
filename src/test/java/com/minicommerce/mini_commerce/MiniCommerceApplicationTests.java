92ed3106d04dfa4d8889c212dd0a7c2bf56b8864
Dann379 <orange_555321@hotmail.com>
2026-02-25 14:28:51 -0600

    Day1: init Spring Boot skeleton (web + thymeleaf)

---
+++ src/test/java/com/minicommerce/mini_commerce/MiniCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minicommerce.mini_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
