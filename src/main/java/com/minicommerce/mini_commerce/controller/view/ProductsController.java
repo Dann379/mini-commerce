eecc47beffd595dade0a7f07e778def70e05e79e
Dann379 <orange_555321@hotmail.com>
2026-03-02 12:09:32 -0600

    "Day3: products page (empty state + table) + navigation"

---
+++ src/main/java/com/minicommerce/mini_commerce/controller/view/ProductsController.java
@@ -0,0 +1,13 @@
+package com.minicommerce.mini_commerce.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductsController {
+    @GetMapping("/products")
+
+    public String products() {
+        return "products";
+    }
+}
